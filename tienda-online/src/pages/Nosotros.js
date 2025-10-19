53b66c6e74b1eb6f75317602cff516a580f88d28
natalia000000 <nataliaparedesbustamante@gmail.com>
2025-10-19 19:30:09 -0300

    avance de la pagina nosotros y muestra de imagenes de la pagina de inicio

--- tienda-online/src/pages/Nosotros.js
+++ tienda-online/src/pages/Nosotros.js
@@ -4,8 +4,14 @@ function Nosotros(){
     return(
         <div className='col-md-4 mb-4'>
             <h4 className='card-title text-primary'>Sobre nosotros: </h4>
-            <p>tenemos una gran variedad de prendas, para todas las temporadas.</p>
-
+            <p>En la tiendita, creemos que la ropa no solo viste, sino que
+            expresa quien eres. Nacimos con la idea de ofrecer prendas unicas,
+            comodas y con estilo </p>
+            <p>tenemos una gran variedad de prendas, de todas la tallas y para todas las temporadas.</p>
+            <h5>Nuestra Misión: </h5>
+            <p>En la tiendita, nuestra mision es ofrecer moda de calidad que haga sentir a
+            cada persona unica y segura de si misma. Creemos que la ropa no solo es un reflejo
+            de estilo, sino también una forma de expresión personal.</p>
         </div>
     )
 }
