3cf561149fd285eadf5b253ba24f68904a69d0b7
natalia000000 <nataliaparedesbustamante@gmail.com>
2025-10-23 18:32:03 -0300

    pagina nosotros y blog

---
+++ tienda-online/src/pages/blog.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+function blog(){
+    return(
+        <div className='container'>
+            <div className="container text-center">
+                <h4>En este blog los los usuarios podran</h4>
+
+                <h5>tiendita@</h5>
+                <p>!Promocion de nuevos productos¡</p>
+                <p>revisa nuestros nuevos productos para el invierno, chaquetas, botines, pantalones, etc.</p>
+
+                <p>PARK PUFFY PARKA</p>
+                <img src="https://www.hellyhansenchile.cl/media/catalog/product/cache/49f931f5369bd606911cd10a2361ac10/5/3/53996_main.jpg"></img>
+
+                <p>Pantalón Vidda Pro Ventilated Mujer</p>
+                <img src="https://fjallraven.cl/cdn/shop/products/7323450528911_fw19_a_vidda_pro_ventilated_trs_w_fjaellraeven_21_5000x.jpg?v=1755792630"></img>
+
+            </div>
+
+
+
+        </div>
+    )
+}
