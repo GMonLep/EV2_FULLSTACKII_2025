1c0a61002ae7eb651798e4b575ec72e8f2279c62
GMonLep <gmonterolepio@gmail.com>
2025-10-23 23:23:02 -0300

    ejemplo producto

--- tienda-online/src/components/ProductosAlmacenados.js
+++ tienda-online/src/components/ProductosAlmacenados.js
@@ -1,8 +1,12 @@
+//los que iran en inicio como destacados
 import destacado1 from '../assets/destacado1.jpg';
 import destacado2 from '../assets/destacado2.jpg';
 import destacado3 from '../assets/destacado3.jpg';
 
+//el resto de productos
 import producto1 from '../assets/producto1.jpg';
+import producto2 from '../assets/producto2.webp';
+import producto3 from '../assets/producto3.webp';
 
 export const ProductosAlmacenados = [
   {
@@ -26,11 +30,29 @@ export const ProductosAlmacenados = [
     categoria: "Ropa mujer",
     img: destacado3
   },
+
   {
     id: 4,
     precio: 25990,
     nombre: "Zapatillas New Balance",
-    categoria: "Zapatillas",
+    categoria: "Calzado",
     img: producto1
   },
+
+  {
+    id: 5,
+    precio: 31990,
+    nombre: "Zapatilla Outdoor Impermeable",
+    categoria: "Calzado",
+    img: producto2
+  },
+
+  {
+    id: 6,
+    precio: 65000,
+    nombre: "Blazer negro",
+    categoria: "Ropa mujer",
+    img: producto3
+  },
+
 ];
