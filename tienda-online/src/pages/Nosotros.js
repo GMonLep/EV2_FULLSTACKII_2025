09591378d090da36b9a41f2a6fcf4199e97bc12a
GMonLep <gmonterolepio@gmail.com>
2025-10-27 15:56:01 -0300

    edicion formato blog y nosotros

--- tienda-online/src/pages/Nosotros.js
+++ tienda-online/src/pages/Nosotros.js
@@ -2,16 +2,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Nosotros(){
     return(
-        <div className='col-md-4 mb-4'>
-            <h4 className='card-title text-primary'>Sobre nosotros: </h4>
-            <p>En la tiendita, creemos que la ropa no solo viste, sino que
+        <div style={{ paddingTop: '0',paddingBottom: '0' ,minHeight: '80vh' }}>
+      {/* SECCION ENCABEZADO */}
+      <section className="bg-primary text-white py-5">
+        <div className="container text-center">
+          <h1 className="display-5 fw-bold"> Sobre nosotros:</h1>
+          <p className="lead">En la tiendita, creemos que la ropa no solo viste, sino que
             expresa quien eres. Nacimos con la idea de ofrecer prendas unicas,
-            comodas y con estilo </p>
-            <p>tenemos una gran variedad de prendas, de todas la tallas y para todas las temporadas.</p>
-            <h5>Nuestra Misión: </h5>
-            <p>En la tiendita, nuestra mision es ofrecer moda de calidad que haga sentir a
-            cada persona unica y segura de si misma. Creemos que la ropa no solo es un reflejo
-            de estilo, sino también una forma de expresión personal.</p>
+            comodas y con estilo.</p>
+            <p className="lead">Tenemos una gran variedad de prendas, de todas la tallas y para todas las temporadas.</p>
+        </div>
+      </section>
+
+      <section className="bg-primary text-white py-5">
+        <div className="container text-center">
+          <h1 className="display-5 fw-bold">Nuestra Misión:</h1>
+          <p className="lead">En la tiendita, nuestra mision es ofrecer moda de calidad que haga sentir a cada persona unica y segura de si misma. Creemos que la ropa no solo es un
+            reflejo de estilo, sino también una forma de expresión personal.</p>
+        </div>
+      </section>
+    
         </div>
     );
 };
